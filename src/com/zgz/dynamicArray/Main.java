718472e97ad5b0f782bda2786aad0857ad938abb
zhou.gz <2327322601@qq.com>
2020-08-21 09:57:28 +0800

    Main 中添加了一些注释  主要是测试git checkout Main是否可行

--- src/com/zgz/dynamicArray/Main.java
+++ src/com/zgz/dynamicArray/Main.java
@@ -2,7 +2,7 @@ package com.zgz.dynamicArray;
 
 public class Main {
     public static void main(String[] args) {
-        MyArrayList1 list = new MyArrayList1();
+        MyArrayList list = new MyArrayList();
         list.add(1);
         list.add(2);
         list.add(3);
@@ -10,5 +10,7 @@ public class Main {
         list.add(5);
         list.add(6);
         System.out.println(list.remove(4));
+
+        //测试 你妹的
     }
 }
