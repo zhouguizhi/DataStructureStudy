132ff23c3be1163b460297e04780e2ce07e55417
zhou.gz <2327322601@qq.com>
2020-08-21 09:44:58 +0800

    git 测试

---
+++ src/com/zgz/dynamicArray/Main.java
@@ -0,0 +1,14 @@
+package com.zgz.dynamicArray;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList1 list = new MyArrayList1();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(list.remove(4));
+    }
+}
