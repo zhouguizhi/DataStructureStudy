fb4f19f01bc478182cbf94c7e964eb1721fdc4f2
zhou.gz <2327322601@qq.com>
2021-05-31 16:19:31 +0800

    添加2个队列实现一个栈题目 还没实现

--- src/com/zgz/leetCode/_16_两个队列实现一个栈.java
+++ src/com/zgz/leetCode/_16_两个队列实现一个栈.java
@@ -2,13 +2,13 @@ package com.zgz.leetCode;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.concurrent.ArrayBlockingQueue;
-
 /**
- * 如何使用栈实现队列的功能
+ * 两个队列实现一个栈
  * 栈特点:先进后出
  * 队列特点:先进先出
+ * 思路:
  */
-public class _16_如何使用栈实现队列的功能 {
+public class _16_两个队列实现一个栈 {
     public static void main(String[] args) {
 //        Stack<Integer> stack = new Stack<>();
 //        Queue queue = new ArrayBlockingQueue();
